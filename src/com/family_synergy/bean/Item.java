3ba1a4f5ea380d3d83cc275d3284dc6e4e179cf8
msssingh0 <singh7179@gmail.com>
2017-08-23 07:04:44 +0530

    Added item class memebers and getter and setters

--- src/com/family_synergy/bean/Item.java
+++ src/com/family_synergy/bean/Item.java
@@ -1,6 +1,45 @@
 package com.family_synergy.bean;
 
+import java.util.Date;
+
 public class Item 
 {
+	private String itemName;
+	private String name;
+	private boolean isDateSet;
+	private Date endDate;
+	private int daysLeft;
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isDateSet() {
+		return isDateSet;
+	}
+	public void setDateSet(boolean isDateSet) {
+		this.isDateSet = isDateSet;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public int getDaysLeft() {
+		return daysLeft;
+	}
+	public void setDaysLeft(int daysLeft) {
+		this.daysLeft = daysLeft;
+	}
 
+	
+	
 }
